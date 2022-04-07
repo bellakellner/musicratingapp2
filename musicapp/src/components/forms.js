fadd5500e9b33b01e3be49c4568b5ba75b6064d2
valerycorral <vcorral@wesleyan.edu>
2022-04-07 14:56:11 -0400

    forms

---
+++ musicapp/src/components/forms.js
@@ -0,0 +1,12 @@
+import React,{ Component } from 'react'
+
+class Form extends Component{
+  render(){
+    return(
+      <div>
+        Form Component
+      </div>
+    )
+  }
+}
+export default Form
