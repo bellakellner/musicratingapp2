f7237399af46b1174dffdc0f6b357555916e7f4c
valerycorral <vcorral@wesleyan.edu>
2022-04-10 20:30:14 -0400

    admin

--- backend/musicapp/admin.py
+++ backend/musicapp/admin.py
@@ -4,8 +4,14 @@ from .models import User, Artist, Rating, Song
 # Register your models here.
 class SongAdmin(admin.ModelAdmin):
     list_display = ('title', 'artist')
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username','password')
+class ArtistAdmin(admin.ModelAdmin):
+    list_display = ('song', 'artist')
+class RatingsAdmin(admin.ModelAdmin):
+    display = ('rating')
 
-admin.site.register(User)
-admin.site.register(Artist)
-admin.site.register(Rating)
+admin.site.register(User,UserAdmin)
+admin.site.register(Artist,ArtistAdmin)
+admin.site.register(Rating,RatingsAdmin)
 admin.site.register(Song,SongAdmin)
